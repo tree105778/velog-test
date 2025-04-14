f34643b8d909bb15ce81bb1d0b20f583c5764211
SUEYOUNG14 <suekim02@naver.com>
2025-04-14 14:54:44 +0900

    공통 헤더 완료

--- src/components/RootLayout.jsx
+++ src/components/RootLayout.jsx
@@ -1,12 +1,10 @@
-import React from 'react';
 import { Outlet } from 'react-router-dom';
 
 function RootLayout() {
   return (
-    <>
+    <div>
       <Outlet />
-      <div>RootLayout</div>
-    </>
+    </div>
   );
 }
 
