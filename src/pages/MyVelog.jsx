357eaf64d1b0bf0f94ee98a1e7d4305b7ad4d444
Jongkyun <hjk326333@gmail.com>
2025-04-15 18:49:16 +0900

    css 수정

--- src/pages/MyVelog.jsx
+++ src/pages/MyVelog.jsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useRef, useState } from 'react';
 import styles from './MyVelog.module.css';
-// import profileImg from '../assets/name.jpg';
 import series from '../assets/series.jpg';
 import introduction from '../assets/Introduction.jpg';
 import Header from '../components/Layout/Header';
@@ -45,11 +44,6 @@ const MyVelog = () => {
       <main className={styles.MyVelogBody}>
         <div className={styles.MyVelogProfileHeader}>
           <div className={styles.MyVelogProfileInfo}>
-            {/* <img
-              src={profileImg}
-              alt="프로필 이미지"
-              className={styles.MyVelogProfileImage}
-            /> */}
             <div className={styles.MyVelogProfile}>{userId}</div>
 
             <h1 className={styles.MyVelogTitle}>{userId}</h1>
@@ -164,6 +158,34 @@ const MyVelog = () => {
                         0개의 댓글
                       </span>
                       <span className={styles.MyVelogDotDivider1}>·</span>
+                      <svg
+                        className={styles.MyVelogLikeImage}
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="20"
+                        height="20"
+                        viewBox="0 0 24 24"
+                        fill="currentColor"
+                      >
+                        <path d="m18 1-6 4-6-4-6 5v7l12 10 12-10V6z" />
+                      </svg>
+                      <span className={styles.MyVelogLikeCount}>0</span>
+                      <span className={styles.MyVelogDotDivider1}>·</span>
+                      <div className={styles.MyVelogPrivateBadge}>
+                        <svg
+                          className={styles.MyVelogPrivateIcon}
+                          xmlns="http://www.w3.org/2000/svg"
+                          fill="none"
+                          viewBox="0 0 24 24"
+                        >
+                          <path
+                            fill="currentColor"
+                            d="M17.625 9H16.5V6.81c0-2.47-1.969-4.522-4.44-4.56a4.514 4.514 0 0 0-4.56 4.5V9H6.375A1.88 1.88 0 0 0 4.5 10.875v9a1.88 1.88 0 0 0 1.875 1.875h11.25a1.88 1.88 0 0 0 1.875-1.875v-9A1.88 1.88 0 0 0 17.625 9zm-4.969 5.85v3.225a.672.672 0 0 1-.623.675.657.657 0 0 1-.69-.656V14.85a1.5 1.5 0 0 1-.838-1.486 1.5 1.5 0 1 1 2.152 1.486zM15.187 9H8.814V6.75c0-.848.332-1.645.937-2.25A3.16 3.16 0 0 1 12 3.562a3.16 3.16 0 0 1 2.25.938 3.16 3.16 0 0 1 .938 2.25V9z"
+                          />
+                        </svg>
+                        <span className={styles.MyVelogPrivateText}>
+                          비공개
+                        </span>
+                      </div>
                     </div>
                   ))
                 ) : (
