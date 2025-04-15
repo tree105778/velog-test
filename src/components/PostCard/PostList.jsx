be3b0dfd147f072ec9cfcd836f912617505e0eae
yeeun-yenny <yeeun6328@gmail.com>
2025-04-15 14:30:50 +0900

    PostList css 추가

--- src/components/PostCard/PostList.jsx
+++ src/components/PostCard/PostList.jsx
@@ -1,18 +1,10 @@
 import data from '../../data/data'; // 경로는 네 위치에 맞게 조정
 import PostCard from './PostCard';
-import dummyPosts from '../../data/data';
 import styles from './PostList.module.css';
 
 const PostList = () => {
   return (
-    <ul
-      style={{
-        padding: '1rem',
-        display: 'grid',
-        gridTemplateColumns: 'repeat(auto-fill, minmax(350px, 1fr))',
-        gap: '1.5rem',
-      }}
-    >
+    <ul className={styles.list}>
       {data.blog_card.map((post, index) => (
         <PostCard key={index} post={post} />
       ))}
